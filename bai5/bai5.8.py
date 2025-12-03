665a9df4134b279ac9e5d9d340f435db0f64b7c1
phamlap1711 <phamlap17112006@gmail.com>
2025-12-03 21:28:38 +0700

    Delete bai5 directory

--- bai5/bai5.8.py
+++
@@ -1,5 +0,0 @@
-def Sequential_Search(dlist, item):
-    for index, value in enumerate(dlist):  
-        if value == item:                  
-            return (True, index)         
-    return (False, -1)                    
